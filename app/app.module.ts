6f0ff262326a648dbdb8d92b889a2e55e720136b
taejong go <gainworld@gmail.com>
2016-11-04 18:41:18 +0900

    chap5 ing....2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,38 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }      		from '@angular/core';
+import { BrowserModule } 		from '@angular/platform-browser';
+import { FormsModule }   		from '@angular/forms';
+import { RouterModule }			from '@angular/router';
 
 import { AppComponent }  		from './app.component';
 import { HeroDetailComponent } 	from './hero-detail.component';
 import { HeroesComponent } 		from './heroes.component';
+import { DashboardComponent }	from './dashboard.component';
 import { HeroService } 			from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+    	{
+			path: 'dashboard',
+			component: DashboardComponent
+		},
+		{
+			path: 'heroes',
+			component: HeroesComponent
+		},
+		{
+			path: '',
+			redirectTo: '/dashboard',
+			pathMatch: 'full'
+		}
+	])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
+    DashboardComponent,
     HeroesComponent
   ],
   providers: [
@@ -22,4 +40,5 @@ import { HeroService } 			from './hero.service';
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
